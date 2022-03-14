624d95f2247ad6183afc577001f6912abf99a67b
JonElf <jonik.elfman@gmail.com>
2022-03-14 18:34:49 +0100

    Added static

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = BASE_DIR / "static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -130,9 +130,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+#]
 
 LOGIN_URL = "/admin/login/"
 
